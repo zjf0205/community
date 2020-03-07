c80233c873624de444fc32b40a02a00a114489eb
e <2436937093@qq.com>
2020-03-07 22:23:23 +0800

    first commit

---
+++ src/main/java/zjf0205/community/CommunityApplication.java
@@ -0,0 +1,13 @@
+package zjf0205.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
