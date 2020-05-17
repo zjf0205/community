ed07a12d0c444f559b01478a5604439c51f5e787
e <2436937093@qq.com>
2020-05-17 23:00:34 +0800

    add question table and publish html

---
+++ src/main/java/zjf0205/community/mapper/QuestionMapper.java
@@ -0,0 +1,12 @@
+package zjf0205.community.mapper;
+
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import zjf0205.community.model.Question;
+
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into question(title,description,gmt_create,gmt_modified,creator,tag ) values(#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+    public void insert(Question question);
+}
